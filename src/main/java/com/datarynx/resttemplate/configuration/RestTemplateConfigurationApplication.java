584889af6cdca26a5ae5c517d34aaac586fb1495
Datarynx <devusers.datarynx@gmail.com>
2020-12-22 19:58:34 +0530

    first commit

---
+++ src/main/java/com/datarynx/resttemplate/configuration/RestTemplateConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.datarynx.resttemplate.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTemplateConfigurationApplication.class, args);
+	}
+
+}
